094e906f20803070c9ec55f13bcb7911c6a920e3
jusales3270 <164698047+jusales3270@users.noreply.github.com>
2025-07-03 07:38:25 -0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,14 @@
-import adapter from '@sveltejs/adapter-node'; // <-- MUDANÇA AQUI
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel'; // 1. Importa o adaptador correto
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(), // <-- MUDANÇA AQUI
-		alias: {
-			$lib: "src/lib",
-			"$lib/*": "src/lib/*",
-			"$components/*": "src/lib/components/*",
-			"$ts/*": "src/lib/ts/*",
-			"$stores/*": "src/lib/stores/*"
-		}
-	}
+    kit: {
+        // 2. Usa o adaptador da Vercel no lugar do antigo
+        adapter: adapter()
+    }
 };
 
 export default config;
